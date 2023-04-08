875ceadd536427c18a21a7c9044fa31c4ae98cbf
Yunus <yunusinne@gmail.com>
2023-04-09 02:02:19 +0300

    components

---
+++ components/footer/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import styles from "./styles.module.css";
+const Footer = () => {
+  return <footer className={styles.footer}>Yunus</footer>;
+};
+
+export default Footer;
